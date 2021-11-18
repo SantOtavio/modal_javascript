f29eb67fe54a3546b8c86d9f945190d2aadfbe9d
TV200 <tiago_volpi@estudante.sc.senai.br>
2021-11-17 21:35:08 -0300

    Primeira parte

---
+++ main.js
@@ -0,0 +1,20 @@
+
+let screen = document.querySelector('body');
+
+let walkBox = document.createElement('div');
+
+walkBox.className = 'walkBox';
+
+
+screen.appendChild(walkBox);
+
+document.querySelector('html').clientHeight
+document.querySelector('html').clientWidth
+
+walkBox.style.width = '40vw';
+walkBox.style.height = '60vh';
+walkBox.style.backgroundColor = 'black';
+walkBox.style.left = '50%';
+function mexerQuadrado(){
+    cont++
+}
---
+++ modal.js
@@ -0,0 +1,63 @@
+
+let screen = document.querySelector('body');
+
+//Criação e estilização do botão para abrir o modal
+
+let buttonforopen = document.createElement('button');
+buttonforopen.textContent = 'ABRIR';
+
+buttonforopen.className = 'buttonforopen';
+
+screen.appendChild(buttonforopen);
+
+//Fazendo o botão ter a função de abrir o modal com transicão
+buttonforopen.onclick = function () {
+    //Criação e estilização do quadrado do modal
+    let walkBox = document.createElement('div');
+    walkBox.className = 'walkBox';
+    screen.appendChild(walkBox);
+    document.querySelector('html').clientHeight
+    document.querySelector('html').clientWidth
+
+    //Criando uma variável para selecionar a tela e dar um background mais escuro
+    let background = document.querySelector('body');
+    buttonforopen.style.top = '-33.6vh';
+    buttonforopen.style.zIndex = '-1';
+    background.style.backgroundColor = 'rgba(0, 0, 0, 0.555)';
+
+    //Criando as divs para dentro do modal 
+    let inputs = document.createElement('div')
+    inputs.className = 'inputs';
+    walkBox.appendChild(inputs);
+
+    let nome = document.createElement('div');
+    nome.className = 'nomecamp';
+    inputs.appendChild(nome);
+
+    let titleInputs = document.createElement('h1')
+    titleInputs.className = 'titleInputs';
+    inputs.appendChild(titleInputs);
+    titleInputs.innerText = 'Faça seu cadastro! '
+
+    let inputNome = document.createElement('input');
+    inputNome.className = 'inputNome';
+    nome.appendChild(inputNome);
+    inputNome.placeholder = 'Nome';
+
+    let inputSobrenome = document.createElement('input');
+    inputSobrenome.className = 'inputSobrenome';
+    nome.appendChild(inputSobrenome);
+    inputSobrenome.placeholder = 'Sobrenome';
+
+    let inputDatanasc = document.createElement('input');
+    inputDatanasc.className = 'inputDatanasc';
+    nome.appendChild(inputDatanasc);
+    inputDatanasc.placeholder = 'Data de nascimento';
+
+    let buttonCadastrar = document.createElement('button');
+    buttonCadastrar.className = 'buttonCadastrar';
+    walkBox.appendChild(buttonCadastrar);
+    buttonCadastrar.textContent = 'Cadastrar'
+}
+
+
