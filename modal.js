6e49929237f925d2596295942e3d861ac95445a2
TV200 <tiago_volpi@estudante.sc.senai.br>
2021-11-18 21:45:25 -0300

    vbçplo

--- modal.js
+++ modal.js
@@ -1,80 +1,121 @@
+let screen = document.querySelector("body");
+let footer = document.querySelector("footer");
 
-let screen = document.querySelector('body');
+let buttonforopen = document.createElement("button");
+buttonforopen.textContent = "ABRIR";
 
-//Criação e estilização do botão para abrir o modal
-
-let buttonforopen = document.createElement('button');
-buttonforopen.textContent = 'ABRIR';
-
-buttonforopen.className = 'buttonforopen';
+buttonforopen.className = "buttonforopen";
 
 screen.appendChild(buttonforopen);
 
-//Fazendo o botão ter a função de abrir o modal com transicão
-buttonforopen.onclick = function () {
-
-    //Criação e estilização do quadrado do modal
-    let walkBox = document.createElement('div');
-    walkBox.className = 'walkBox';
+buttonforopen.onclick = function() {
+    let walkBox = document.createElement("div");
+    walkBox.className = "walkBox";
     screen.appendChild(walkBox);
-    document.querySelector('html').clientHeight
-    document.querySelector('html').clientWidth
 
-    //Criando uma variável para selecionar a tela e dar um background mais escuro
-    let background = document.querySelector('body');
-    buttonforopen.style.top = '-33.6vh';
-    buttonforopen.style.zIndex = '-1';
-    background.style.backgroundColor = 'rgba(0, 0, 0, 0.555)';
+    let background = document.querySelector("body");
+    background.style.backgroundColor = "rgba(0, 0, 0, 0.555)";
 
-    //Criando as divs para dentro do modal 
-    let inputs = document.createElement('div')
-    inputs.className = 'inputs';
+    let inputs = document.createElement("div");
+    inputs.className = "inputs";
     walkBox.appendChild(inputs);
 
-    let nome = document.createElement('div');
-    nome.className = 'nomecamp';
+    let nome = document.createElement("div");
+    nome.className = "nomecamp";
     inputs.appendChild(nome);
 
-    let titleInputs = document.createElement('h1')
-    titleInputs.className = 'titleInputs';
+    let titleInputs = document.createElement("h1");
+    titleInputs.className = "titleInputs";
     inputs.appendChild(titleInputs);
-    titleInputs.innerText = 'Faça seu cadastro! '
+    titleInputs.innerText = "Faça seu cadastro! ";
 
-    let inputNome = document.createElement('input');
-    inputNome.className = 'inputNome';
+    let inputNome = document.createElement("input");
+    inputNome.className = "inputNome";
     nome.appendChild(inputNome);
-    inputNome.placeholder = 'Nome';
+    inputNome.placeholder = "Nome";
 
-    let inputSobrenome = document.createElement('input');
-    inputSobrenome.className = 'inputSobrenome';
+    let inputSobrenome = document.createElement("input");
+    inputSobrenome.className = "inputSobrenome";
     nome.appendChild(inputSobrenome);
-    inputSobrenome.placeholder = 'Sobrenome';
+    inputSobrenome.placeholder = "Sobrenome";
 
-    let inputDatanasc = document.createElement('input');
-    inputDatanasc.className = 'inputDatanasc';
+    let inputDatanasc = document.createElement("input");
+    inputDatanasc.type = "date";
+    inputDatanasc.className = "inputDatanasc";
     nome.appendChild(inputDatanasc);
-    inputDatanasc.placeholder = 'Data de nascimento';
+    inputDatanasc.placeholder = "Data de nascimento";
 
-    let buttonCadastrar = document.createElement('button');
-    buttonCadastrar.className = 'buttonCadastrar';
+    let buttonCadastrar = document.createElement("button");
+    buttonCadastrar.className = "buttonCadastrar";
     walkBox.appendChild(buttonCadastrar);
-    buttonCadastrar.textContent = 'Cadastrar'
+    buttonCadastrar.textContent = "Cadastrar";
 
-    let buttonCancelar = document.createElement('button');
-    buttonCancelar.className = 'buttonCancelar';
+    let buttonCancelar = document.createElement("button");
+    buttonCancelar.className = "buttonCancelar";
     walkBox.appendChild(buttonCancelar);
-    buttonCancelar.textContent = 'Cancelar'
+    buttonCancelar.textContent = "Cancelar";
 
-
-    buttonCancelar.onclick = function () {
+    buttonCancelar.onclick = function() {
         walkBox.remove();
-        background.remove();
+        background.style.backgroundColor = "transparent";
+    };
 
-        let buttonforopenDENOVO = document.createElement('button');
-        buttonforopenDENOVO.textContent = 'ABRIR';
-        buttonforopenDENOVO.className = 'buttonforopenDENOVO';
-        screen.appendChild(buttonforopenDENOVO);
-    }
-}
+    buttonCadastrar.onclick = function() {
+        let nome = document.querySelector("inputNome");
+        nome = inputNome.value;
+        let sobrenome = document.querySelector("inputSobrenome");
+        sobrenome = inputSobrenome.value;
+        let dataNasc = document.querySelector("inputDatanasc");
+        dataNasc = inputDatanasc.value;
 
+        if (!nome || nome == "") {
+            let mensagemVermelha = document.createElement("div");
+            mensagemVermelha.className = "mensagemVermelha";
+            screen.appendChild(mensagemVermelha);
+            mensagemVermelha.innerText =
+                "Preencha todos os campos ou clique em cancelar para sair!";
+        } else if (!sobrenome || sobrenome == "") {
+            let mensagemVermelha = document.createElement("div");
+            mensagemVermelha.className = "mensagemVermelha";
+            screen.appendChild(mensagemVermelha);
+            mensagemVermelha.innerText =
+                "Preencha todos os campos ou clique em cancelar para sair!";
+        } else if (!dataNasc || dataNasc == "") {
+            let mensagemVermelha = document.createElement("div");
+            mensagemVermelha.className = "mensagemVermelha";
+            screen.appendChild(mensagemVermelha);
+            mensagemVermelha.innerText =
+                "Preencha todos os campos ou clique em cancelar para sair!";
+        } else {
+            let listagemDados = document.createElement("div");
+            listagemDados.className = "listagemDados";
+            screen.appendChild(listagemDados);
+            let mensagemVerde = document.createElement("div");
+            mensagemVerde.className = "mensagemVerde";
+            screen.appendChild(mensagemVerde);
+            mensagemVerde.innerText = "Cadastro realizado!";
+            setTimeout(function() {
+                mensagemVerde.remove();
+                walkBox.remove();
+                background.style.backgroundColor = "transparent";
+            }, 3000);
 
+            cadastroPessoa = function() {
+                nome = document.querySelector(".inputNome");
+                valorNome = nome.value;
+
+                sobrenome = document.querySelector(".inputSobrenome");
+                valorSobrenome = sobrenome.value;
+
+                data = document.querySelector(".inputDatanasc");
+                valorData = data.value;
+                let pessoa = {
+                    nome: valorNome,
+                    sobrenome: valorSobrenome,
+                    data: valorData,
+                };
+                listaPessoa = [pessoa];
+            };
+        }
+    };
+};
